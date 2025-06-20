12da9c8f6b960d2154e84938449d592372aca3e3
Fatih Seida <35951720+FatihSeida@users.noreply.github.com>
2025-06-21 04:15:18 +0700

    Delete depgraph_hsic_only directory

--- depgraph_hsic_only/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Public API for the depgraph_hsic_only package."""
-
-from .pruner_base import Yolov8SegPruner
-from .yolov8_pruner import DefaultYolov8SegPruner
-
-__all__ = [
-    "Yolov8SegPruner",
-    "DefaultYolov8SegPruner",
-]
--- depgraph_hsic_only/__main__.py
+++
@@ -1,7 +0,0 @@
-"""Package entry point."""
-
-from .cli import main
-
-
-if __name__ == "__main__":  # pragma: no cover - package entrypoint
-    main()
