66910251b984cff5cc5108abfb2deecbc3e0aeed
Fatih Seida <35951720+FatihSeida@users.noreply.github.com>
2025-06-20 20:08:54 +0700

    Handle older Ultralytics imports

--- depgraph_hsic_only/yolov8_pruner.py
+++ depgraph_hsic_only/yolov8_pruner.py
@@ -34,7 +34,13 @@ from ultralytics.nn.modules.block import C2f, Bottleneck
 from ultralytics.nn.modules.conv import Conv
 from ultralytics.nn.tasks import attempt_load_one_weight
 from ultralytics.engine.trainer import BaseTrainer
-from ultralytics.utils import yaml_load, LOGGER, RANK, DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS
+try:  # pragma: no cover - fallback for older Ultralytics versions
+    from ultralytics.utils import yaml_load, DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS
+except Exception:  # pragma: no cover
+    from ultralytics.utils.files import yaml_load
+    from ultralytics.cfg import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS
+
+from ultralytics.utils import LOGGER, RANK
 from ultralytics.utils.checks import check_yaml
 from ultralytics.utils.torch_utils import initialize_weights, de_parallel
 
